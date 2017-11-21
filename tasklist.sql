39819f8fcc36b0d58f39ab603c7326430ba15a7e
LAPTOP-OGA6UVAR\Vu Dong <hoangvudp@gmail.com>
2017-11-21 16:46:06 -0600

    New User table in DB

--- tasklist.sql
+++ tasklist.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 21/11/2017 13:41:26
+ Date: 21/11/2017 16:45:22
 */
 
 SET NAMES utf8mb4;
@@ -30,14 +30,34 @@ CREATE TABLE `task`  (
   `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of task
 -- ----------------------------
-INSERT INTO `task` VALUES (1, 'Test', '2017-11-20', 'Personal', 'Normal', 'Created', 1);
-INSERT INTO `task` VALUES (2, 'Abc def', '2017-11-21', 'Personal', 'High', 'Created', 2);
-INSERT INTO `task` VALUES (5, '345345345345', '2017-11-21', 'Work', 'Lowest', 'Completed', 2);
-INSERT INTO `task` VALUES (6, 'Go to bed', '2017-11-21', 'Work', 'Urgent', 'Created', 1);
+INSERT INTO `task` VALUES (19, '456', '2017-11-21', 'Personal', 'Lowest', 'Completed', 1);
+INSERT INTO `task` VALUES (20, 'aaaaaaaaaaa', '2017-11-21', 'Work', 'Urgent', 'Completed', 1);
+INSERT INTO `task` VALUES (21, 'bbbbbbbb', '2017-11-21', 'Personal', 'Normal', 'Completed', 1);
+INSERT INTO `task` VALUES (23, 'qqqqqqqqq', '2017-11-21', 'Personal', 'Critical', 'Created', 1);
+INSERT INTO `task` VALUES (25, 'qqqqqqqqqqqqqq', '2017-11-21', 'Work', 'Urgent', 'Created', 2);
+INSERT INTO `task` VALUES (26, 'rrrrrrrrrr', '2017-11-21', 'Work', 'Low', 'Created', 1);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'Pham Dong', 'phvdong@mum.edu');
+INSERT INTO `user` VALUES (2, 'Vinh Hoang', 'hthoang@mum.edu');
+INSERT INTO `user` VALUES (3, 'Yongzhao Zhang\r\n', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
