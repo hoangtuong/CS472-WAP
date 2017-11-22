79444e85daea0860661eefa92aa340784984f230
Tuong Hoang <hoangvinhtuong@gmail.com>
2017-11-21 20:49:18 -0600

    Update init db

--- tasklist.sql
+++ tasklist.sql
@@ -1,63 +1,60 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : localhost
+ Source Server         : TaskList
  Source Server Type    : MySQL
  Source Server Version : 50720
- Source Host           : localhost:3306
- Source Schema         : tasklist
+ Source Host           : localhost
+ Source Database       : TaskList
 
  Target Server Type    : MySQL
  Target Server Version : 50720
- File Encoding         : 65001
+ File Encoding         : utf-8
 
- Date: 21/11/2017 16:45:22
+ Date: 11/21/2017 20:47:41 PM
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for task
+--  Table structure for `task`
 -- ----------------------------
 DROP TABLE IF EXISTS `task`;
-CREATE TABLE `task`  (
+CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `task` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `dueDate` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `priority` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `task` varchar(255) DEFAULT NULL,
+  `dueDate` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `priority` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
--- Records of task
+--  Records of `task`
 -- ----------------------------
-INSERT INTO `task` VALUES (19, '456', '2017-11-21', 'Personal', 'Lowest', 'Completed', 1);
-INSERT INTO `task` VALUES (20, 'aaaaaaaaaaa', '2017-11-21', 'Work', 'Urgent', 'Completed', 1);
-INSERT INTO `task` VALUES (21, 'bbbbbbbb', '2017-11-21', 'Personal', 'Normal', 'Completed', 1);
-INSERT INTO `task` VALUES (23, 'qqqqqqqqq', '2017-11-21', 'Personal', 'Critical', 'Created', 1);
-INSERT INTO `task` VALUES (25, 'qqqqqqqqqqqqqq', '2017-11-21', 'Work', 'Urgent', 'Created', 2);
-INSERT INTO `task` VALUES (26, 'rrrrrrrrrr', '2017-11-21', 'Work', 'Low', 'Created', 1);
+BEGIN;
+INSERT INTO `task` VALUES ('19', 'Setup/configure MySQL', '2017-11-28', 'Work', 'High', 'Created', '1'), ('20', 'Database MySQL: create a table Task', '2017-11-28', 'Work', 'Urgent', 'Created', '1'), ('21', 'Server side - Implement Database connection', '2017-11-27', 'Work', 'Normal', 'Created', '1'), ('23', 'Server side - Implement Database APIs: get task list, update task, insert task', '2017-11-26', 'Work', 'Critical', 'Created', '1'), ('25', 'Support to delete a task in both client and server	', '2017-11-29', 'Work', 'Urgent', 'Created', '1'), ('26', 'Support to mark a Task as Complete	', '2017-11-30', 'Work', 'Low', 'Created', '1'), ('27', 'Setup Github repository, code base', '2017-11-27', 'Work', 'High', 'Created', '2'), ('28', 'Add \'priority\' field (both front end and backend sides)', '2017-11-26', 'Work', 'Critical', 'Created', '2'), ('30', 'Sort task list by due date and priority	', '2017-11-25', 'Work', 'Critical', 'Created', '2'), ('31', 'Add userId to form', '2017-11-26', 'Work', 'Critical', 'Created', '3'), ('32', 'Config mysql on my own local machine', '2017-11-25', 'Work', 'Normal', 'Created', '3'), ('33', 'Sleep well', '2017-11-20', 'Personal', 'Normal', 'Completed', '2'), ('34', 'test', '2017-11-11', 'Personal', 'Critical', 'Created', '2');
+COMMIT;
 
 -- ----------------------------
--- Table structure for user
+--  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
--- Records of user
+--  Records of `user`
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'Pham Dong', 'phvdong@mum.edu');
-INSERT INTO `user` VALUES (2, 'Vinh Hoang', 'hthoang@mum.edu');
-INSERT INTO `user` VALUES (3, 'Yongzhao Zhang\r\n', NULL);
+BEGIN;
+INSERT INTO `user` VALUES ('1', 'Pham Dong', 'phvdong@mum.edu'), ('2', 'Vinh Hoang', 'hthoang@mum.edu'), ('3', 'Yongzhao Zhang\r\n', 'yzhang@mum.edu');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
